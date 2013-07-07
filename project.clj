eb7101ddb34d1c2b930b86102dfce5132076f0e9
tiredpixel <tp@tiredpixel.com>
2013-07-07 17:22:26 +0100

    Bump version to v0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mtrx9 "0.1.0-SNAPSHOT"
+(defproject mtrx9 "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
