0511e7c5ae7b070eb5a4064390022be0bd4ff2c9
tiredpixel <tp@tiredpixel.com>
2013-07-07 17:43:33 +0100

    Fix mtrx9.core tests.

--- test/mtrx9/test/core.clj
+++ test/mtrx9/test/core.clj
@@ -1,13 +1,13 @@
-(ns mtrx9.test.handler
+(ns mtrx9.test.core
   (:use clojure.test
         ring.mock.request  
-        mtrx9.handler))
+        mtrx9.core))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:body response) "MTRX9"))))
   
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
