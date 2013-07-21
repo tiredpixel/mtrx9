cc292eb6abe8ca5b96e140a0cab6f42fc31a8d41
tiredpixel <tp@tiredpixel.com>
2013-07-21 17:10:17 +0100

    Improve textual content.

--- src/mtrx9/views/statics.clj
+++ src/mtrx9/views/statics.clj
@@ -7,11 +7,10 @@
     [:div
       [:h1 "MTRX9"]
       [:p "Keep an eye on many things."]
-      [:p "1 character at a time."]
-      [:p "3 dimensions:"]
+      [:p "Simple monitoring in 3 dimensions:"]
       [:ul
         [:li "streams"]
-        [:li "values"]
+        [:li "chars"]
         [:li "time"]]
       [:div
         (form-to [:post "/matrices"]
@@ -21,7 +20,7 @@
   (layout/sidebar-right "About | MTRX9"
     [:div
       [:h1 "About"]
-      [:p "MTRX9 is written in Clojure."]
+      [:p "MTRX9 is written in Clojure. It uses Websockets."]
       [:p "Fork it on " [:a {:href "https://github.com/tiredpixel/mtrx9"} "GitHub"] "."]
       [:p "Tweet " [:a {:href "https://twitter.com/mtrx9"} "@mtrx9"] "."]
       [:p "More sleep lost by " [:a {:href "http://www.tiredpixel.com"} "tiredpixel"] "."]]))
