f95c006239fff60e41c35b344c1b3303b9c38075
tiredpixel <tp@tiredpixel.com>
2013-07-21 15:37:11 +0100

    Implement POST /matrices/:id, with update,websocket-init,websocket-handler.
    Create fully-working websockets implementation (server-side only),
    receiving `POST /matrices/:id` and relaying it to websockets. aleph
    named-channels are used, with `:id` the name of each channel. Thus,
    communication is kept segregated.

--- src/mtrx9/controllers/matrices.clj
+++ src/mtrx9/controllers/matrices.clj
@@ -1,5 +1,8 @@
 (ns mtrx9.controllers.matrices
-  (:use [compojure.core :only (defroutes GET POST)])
+  (:use [compojure.core :only (defroutes GET POST)]
+        lamina.core
+        aleph.http
+        aleph.formats)
   (:require [ring.util.response :as ring]
             [mtrx9.views.matrices :as view]))
 
@@ -10,14 +13,32 @@
 (defn- rand-matrix-id []
   (clojure.string/join (repeatedly id-length #(rand-nth id-chars))))
 
+(defn websocket-init [channel]
+  (receive-all channel #(println "C:" %)))
+
+(defn websocket-handler [channel request]
+  (let [matrix-id (get-in request [:route-params :id])
+        matrix-channel (named-channel matrix-id websocket-init)]
+    (siphon matrix-channel channel)
+    (siphon channel matrix-channel)))
+
 (defn show [id]
   (view/show id))
 
 (defn create []
   (ring/redirect (str "/matrices/" (rand-matrix-id))))
 
+(defn update [id chars]
+  (println "U:" id chars)
+  (enqueue (named-channel id websocket-init) (encode-json->string chars))
+  {:status 204})
+
 (defroutes routes
+  (GET ["/matrices/:id/websocket" :id id-re] []
+    (wrap-aleph-handler websocket-handler))
   (GET ["/matrices/:id" :id id-re] [id]
     (show id))
+  (POST ["/matrices/:id" :id id-re] [id chars]
+    (update id chars))
   (POST "/matrices" []
     (create)))
